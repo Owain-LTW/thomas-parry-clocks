51e874785b945fd56e2cfda69d72a284b968f3fb
Owain Walker <owain.walker@capgemini.com>
2023-09-11 21:05:00 +0100

    Another initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 function App() {
   return (
-    <div className="flex-container">
-      <div className="content">
-        <h1>Thomas Parry Clocks</h1>
-        <p>Website coming soon!</p>
-        <p>Gwefan yn dod cyn bo hir!</p>
-      </div>
+    <div>
+      
     </div>
   );
 }
