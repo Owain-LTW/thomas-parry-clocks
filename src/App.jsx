6a3f0ac4afbad62f54cc7f476808c251e4ce5702
Owain Walker <owain.walker@capgemini.com>
2023-09-11 22:04:35 +0100

    wip homepage hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <section id="hero">
+        <div id="hero-padding" className="text-center">
+          <h1 className="text-impact">Watch, Clock and Chronometer</h1>
+          <h2 className="text-impact">REPAIRS IN ABERYSTWYTH</h2>
+        </div>
+      </section>
+      <section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente totam
+        esse quisquam dolore quo earum rem corporis? Modi libero quas dolor
+        aperiam accusantium dicta dolorum natus ducimus, vel praesentium nihil?
+      </section>
+    </>
   );
 }
 
